30ffd4bb8746726bbb860b46239bd2fd914f2683
Rudresh Asagodu <rasagodu.work@gmail.com>
2026-08-17 09:54:14 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int L=0;
+        int R= s.size()-1;
+        while(L<R){
+            if(!isalnum(s[L]))L++;
+            else if(!isalnum(s[R]))R--;
+            else{
+            if(tolower(s[L])!=tolower(s[R])) return false;
+            L++;
+            R--;
+            }
+        }
+        return true;
+    }
+
+};
