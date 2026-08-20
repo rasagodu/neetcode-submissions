b6fcfaee4781c7e27a153dbc4f31c5b53b551d65
Rudresh Asagodu <rasagodu.work@gmail.com>
2026-08-20 07:03:16 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.empty()) return false;
+        if(s[0]==']'|| s[0]=='}'|| s[0]==')') return false;
+        vector<int> signs;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='{' || s[i]=='[' || s[i]=='('){
+                signs.push_back(s[i]);
+            }
+            else if(s[i]==')'){
+                if(signs.empty() || signs[signs.size()-1] !='(') return false;
+                signs.pop_back();
+            }
+            else if(s[i]==']'){
+                if(signs.empty() || signs[signs.size()-1] !='[') return false;
+                signs.pop_back();
+            }
+            else if(s[i]=='}'){
+                if(signs.empty() || signs[signs.size()-1] !='{') return false;
+                signs.pop_back();
+            }
+        }
+        if(signs.size()!=0)return false;
+        return true;
+          
+    }
+};
