96d88c535c632ff6d6ef018c8d16bd6de7e2eaf7
Rudresh Asagodu <rasagodu.work@gmail.com>
2026-08-25 17:54:08 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+       vector<int> res(temperatures.size(),0);
+       stack<int> index;
+       for(int i=0;i<temperatures.size();i++){
+        if(index.empty()|| temperatures[i]<= temperatures[i-1]) index.push(i);
+        else{
+                while(!index.empty() && temperatures[index.top()]<temperatures[i]){
+                    res[index.top()] = i-index.top();
+                    index.pop();
+                }
+                index.push(i);
+            }
+       }
+       return res;
+    }
+};
