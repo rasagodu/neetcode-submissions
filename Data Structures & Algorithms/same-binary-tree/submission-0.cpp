02e2299e269df835cfc7f6a602e2b17629c92dcd
Rudresh Asagodu <rasagodu.work@gmail.com>
2026-09-25 07:43:53 +0530

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<TreeNode*>  nodes;
+        if(!p && !q) return true;
+        else if((!p && q) || (p && !q)) return false;
+        
+       if(p && q){
+            nodes.push(p);
+            nodes.push(q);
+       }
+       while(nodes.size()>0){
+            int length = nodes.size();
+            for(int i =0; i<length/2;i++){
+                TreeNode* curr = nodes.front();
+                nodes.pop();
+                TreeNode* curr2 = nodes.front();
+                nodes.pop();
+                if(curr -> val!= curr2 -> val) return false;
+                if(curr -> left && curr2 -> left){
+                    nodes.push(curr->left);
+                    nodes.push(curr2->left);
+                }
+                else if((curr -> left && !curr2 -> left) || (!curr -> left && curr2 -> left)) return false;
+                if(curr -> right && curr2 -> right){
+                    nodes.push(curr -> right);
+                    nodes.push(curr2 -> right);
+                }
+                 else if((curr -> right && !curr2 -> right) || (!curr -> right && curr2 -> right)) return false;
+                
+            }
+       }
+       return true;
+       
+    }
+
+};
